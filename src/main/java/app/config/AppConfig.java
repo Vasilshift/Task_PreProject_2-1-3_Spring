398c1c29a243cc056f2fe1af983bc0d94ce4073b
Evgeny <vasilkalin@gmail.com>
2021-07-05 21:25:29 +0200

    Work variant. Deleted new timer.

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -10,14 +10,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "app")
 public class AppConfig {
 
-//    @Bean
-//    public Cat createCat() {
-//        return new Cat();
-//    }
-//
-//    @Bean
-//    public Dog createDog() {
-//        return new Dog();
-//    }
 
 }
